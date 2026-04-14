6a73602c333e2ec706a201857902ca55af5ad300
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-04-14 12:07:57 -0300

    dfg

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ mypage ]
 
   # GET /jobs or /jobs.json
   def index
